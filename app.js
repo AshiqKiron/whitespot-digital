5b615932d1070075cc0bafd237c3b14faa483e56
ashiq <ashiq.kiron@gmail.com>
2017-09-09 13:23:06 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+        });
+
+
+if ($('#back-to-top').length) {
+    var scrollTrigger = 1200, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 900);
+    });
+
+}
+
+
+});
+
+/* Disable map scrolling by default*/
+$('.map-container')
+  .click(function(){
+      $(this).find('iframe').addClass('clicked')})
+  .mouseleave(function(){
+      $(this).find('iframe').removeClass('clicked')});
